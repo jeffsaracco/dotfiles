4032bc3fdbc745f29add687059fe7d8f3c046f16
Jeff Saracco <jeffsaracco@github.com>
2021-01-24 09:18:50 -0500

    Add missing stuff

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env zsh
 
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 ln -sf ~/dotfiles/home/zsh/zshrc ~/.zshrc
 ln -sf ~/dotfiles/home/git/gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/home/git/gitignore_global ~/.gitignore_global
@@ -10,6 +12,9 @@ ln -sf ~/dotfiles/home/atom ~/.atom
 ln -sf ~/dotfiles/home/noti/noti.yml ~/.noti.yaml
 
 ln -sf ~/dotfiles/diff-so-fancy/diff-so-fancy ~/dotfiles/bin/diff-so-fancy
+
+ln -sf ~/dotfiles/home/dotbin/tmuxinator.zsh ~/bin/tmuxinator.zsh
+ln -sf ~/dotfiles/home/p10k.zsh ~/.p10k.zsh
 
 # Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
