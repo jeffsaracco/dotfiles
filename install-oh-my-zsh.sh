6319d25ed38040790c2633f976e94bb588d68298
Jeff Saracco <jeffsaracco@github.com>
2021-09-10 08:46:13 -0400

    Lots of clean up

---
+++ install-oh-my-zsh.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+if [[ ! -d ~/.oh-my-zsh ]]
+then
+  ZSH= sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+  echo "oh-my-zsh is already installed"
+fi
