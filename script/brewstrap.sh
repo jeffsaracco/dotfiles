051a89a0492243f86bd1f73f66af4572b7228f5d
Jeff Saracco <jeffsaracco@github.com>
2022-04-27 10:31:04 -0400

    Don't install fd-find, since brew is working, then install brew deps

--- script/brewstrap.sh
+++ script/brewstrap.sh
@@ -20,4 +20,8 @@ if [ ! -d "$HOMEBREW_LOCATION" ]; then
   NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
+if [[ "$CODESPACES" = "true" ]]; then
+  sudo apt-get install build-essential procps curl file git
+fi
+
 $(HOMEBREW_LOCATION)/brew bundle install --no-lock --file $(BREWFILE)
--- script/codespaces-setup.sh
+++ script/codespaces-setup.sh
@@ -3,12 +3,8 @@
 set -e
 
 if [[ "$CODESPACES" = "true" ]]; then
-  sudo apt install fd-find
-
   sudo apt install ruby-dev
   sudo gem install solargraph
 
   sudo chsh -s /usr/bin/zsh
-
-  sudo apt-get install build-essential procps curl file git
 fi
