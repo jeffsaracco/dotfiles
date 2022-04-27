9b211e4fd7ffa27f9bb8e830e56839a92a33f51f
Jeff Saracco <jeffsaracco@github.com>
2022-04-27 10:13:14 -0400

    Some codespaces updates

---
+++ script/brewstrap.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+BREWFILE=../Brewfile
+
+OS="$(uname -s)"
+
+if [ "$OS" = "Darwin")
+  HOMEBREW_LOCATION=/usr/local/bin
+else if [ "$OS" = "Linux")
+  HOMEBREW_LOCATION=/home/linuxbrew/.linuxbrew/bin
+fi
+
+if [ ! -d "$HOMEBREW_LOCATION" ]; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
+$(HOMEBREW_LOCATION)/brew bundle install --no-lock --file $(BREWFILE)
---
+++ script/codespaces-setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ "$CODESPACES" = "true" ]; then
+  sudo apt install fd-find
+
+  sudo apt install ruby-dev
+  sudo gem install solargraph
+
+  sudo chsh -s /usr/bin/zsh
+
+  sudo apt-get install build-essential procps curl file git
+fi
